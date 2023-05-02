211ea743aec95779919d78afe8222dc4446fdd50
loomedai <bbrith@hotmail.com>
2023-05-02 13:44:38 +0200

    Pris for sig selv

--- react-frontend/src/components/Card.js
+++ react-frontend/src/components/Card.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 const Card = () => {
   const [posts, setPosts] = useState([]);
 
@@ -8,30 +9,55 @@ const Card = () => {
       .then((res) => setPosts(res.data));
   }, []);
 
-  const postCards = posts.map((post) => (
-    <div key={post.id} className="card card-compact w-72 bg-base-500 shadow-xl">
-      <div className="card-body rounded-lg shadow-lg h-full">
-        <div className="p-4 h-full">
-          <div className="my-4">
+  const removePrisParagraph = (html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const prisElement = doc.getElementById('pris');
+
+    if (prisElement) {
+      prisElement.remove();
+    }
+
+    return doc.body.innerHTML;
+  };
+
+  const extractPriceFromPrisParagraph = (html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const prisElement = doc.getElementById('pris');
+
+    if (prisElement) {
+      return prisElement.textContent;
+    }
+
+    return '';
+  };
+
+  const postCards = posts.map((post) => {
+    const contentWithoutPris = removePrisParagraph(post.content.rendered);
+    const price = extractPriceFromPrisParagraph(post.content.rendered);
+
+    return (
+      <div key={post.id} className="card card-compact w-72 bg-base-500 shadow-xl">
+        <div className="card-body rounded-lg shadow-lg h-full">
+          <div className="p-4 h-full">
             <h2 className="card-title text-lg font-bold max-h-20">{post.title.rendered}</h2>
             <div
               className="text-white text-base mt-2"
-              dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+              dangerouslySetInnerHTML={{ __html: contentWithoutPris }}
             />
+            <div className="price mt-4">{price}</div>
           </div>
         </div>
       </div>
-    </div>
-  ));
-
-
+    );
+  });
 
   return (
     <>
-      <div className="flex flex-wrap justify-center m-6 ">{postCards}</div>
-      
+      <div className="flex flex-wrap justify-center m-6">{postCards}</div>
     </>
   );
-}
+};
 
 export default Card;

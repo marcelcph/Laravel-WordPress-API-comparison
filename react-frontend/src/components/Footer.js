2bb1ac4bbadc50557efee50fcc3e9e745dc1adb8
Bnop1146 <79976803+Bnop1146@users.noreply.github.com>
2023-05-04 22:27:32 +0200

    Nav og Footer på Engelsk

--- react-frontend/src/components/Footer.js
+++ react-frontend/src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
     <>
       <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <div class="grid grid-flow-col gap-4">
-          <Link to="/"><a class="link link-hover">Forside</a></Link>
-          <Link to="/shop"><a class="link link-hover">Produkter</a></Link>
-          <Link to="/cart"><a class="link link-hover">Kurv</a></Link>
+          <Link to="/"><a class="link link-hover">Home</a></Link>
+          <Link to="/shop"><a class="link link-hover">Products</a></Link>
+          <Link to="/cart"><a class="link link-hover">Cart</a></Link>
         </div>
         <div>
           <p>Copyright © 2023 - All right reserved by Shoe'Fix Aps</p>
